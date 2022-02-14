814190e9eb1f85a887fd4a2dd7b68b024f29cfe5
thekingofpower <huntermjack@gmail.com>
2022-02-14 16:38:00 -0500

    Updated some stuff for AutoDriveSubsystem and added AutoConstants

---
+++ src/main/java/frc/robot/AutoConstants.java
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class AutoConstants {
+    public static int AUTO_PID_LOOP_IDX = 0;//change maybe
+    public static int AUTO_TIMEOUT_MS = 5;//change maybe
+    public static Gains aGains = new Gains(0.18, 0.0, 0.09, 0.047,  0,  1.0);
+}
--- src/main/java/frc/robot/subsystems/AutoDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/AutoDriveSubsystem.java
@@ -26,7 +26,11 @@ import frc.robot.Constants;
 
 public class AutoDriveSubsystem extends SubsystemBase {
   /** Creates a new AutoDriveSubsystem. */
-  //private static TalonFX shooterMotor = new TalonFX(Constants.SHOOTER_MOTOR_PORT);
+  /*private static TalonFX leftFrontDriveMotor = new TalonFX(Constants.LEFT_FRONT_DRIVE_MOTOR_PORT);
+  private static TalonFX leftBackDriveMotor = new TalonFX(Constants.LEFT_BACK_DRIVE_MOTOR_PORT);
+  private static TalonFX rightFrontDriveMotor = new TalonFX(Constants.RIGHT_FRONT_DRIVE_MOTOR_PORT);
+  private static TalonFX rightBackDriveMotor = new TalonFX(Constants.RIGHT_BACK_DRIVE_MOTOR_PORT);*/
+
   /*
   
 double rpm = 6380;
@@ -37,21 +41,21 @@ double rpm = 6380;
  NetworkTableEntry ty = table.getEntry("ty");
   */
   public AutoDriveSubsystem() {
-    /*shooterMotor.setNeutralMode(NeutralMode.Coast);
+    /*leftFrontDriveMotor.setNeutralMode(NeutralMode.Coast);
 
-    shooterMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, Constants.PID_LOOP_IDX, Constants.TIMEOUT_MS);
-    shooterMotor.setSensorPhase(Constants.PHASE_SENSOR);
-    shooterMotor.configNominalOutputForward(0, Constants.TIMEOUT_MS);
-		shooterMotor.configNominalOutputReverse(0, Constants.TIMEOUT_MS);
-		shooterMotor.configPeakOutputForward(Constants.kGains.kPeakOutputShooter, Constants.TIMEOUT_MS);
-		shooterMotor.configPeakOutputReverse(-Constants.kGains.kPeakOutputShooter, Constants.TIMEOUT_MS);
+    leftFrontDriveMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, Constants.AUTO_PID_LOOP_IDX, ConstAutoConstants_AUTO_TIMEOUT_MS);
+    leftFrontDriveMotor.setSensorPhase(Constants.PHASE_SENSOR);
+    leftFrontDriveMotor.configNominalOutputForward(0, AutoConstants.AUTO_TIMEOUT_MS);
+		leftFrontDriveMotor.configNominalOutputReverse(0, AutoConstants.AUTO_TIMEOUT_MS);
+		leftFrontDriveMotor.configPeakOutputForward(Constants.kGains.kPeakOutputShooter, AutoConstants.AUTO_TIMEOUT_MS);
+		leftFrontDriveMotor.configPeakOutputReverse(-Constants.kGains.kPeakOutputShooter, AutoConstants.AUTO_TIMEOUT_MS);
 
-		shooterMotor.configAllowableClosedloopError(Constants.PID_LOOP_IDX, (Constants.K_SENSOR_UNITS_PER_ROTATION / 600.0) * Constants.SHOOTER_ERROR, Constants.TIMEOUT_MS);
+		leftFrontDriveMotor.configAllowableClosedloopError(AutoConstants.AUTO_PID_LOOP_IDX, (Constants.K_SENSOR_UNITS_PER_ROTATION / 600.0) * Constants.SHOOTER_ERROR, AutoConstants.AUTO_TIMEOUT_MS);
 
-		shooterMotor.config_kF(Constants.PID_LOOP_IDX, Constants.kGains.kFShooter, Constants.TIMEOUT_MS);  
-    shooterMotor.config_kP(Constants.PID_LOOP_IDX, Constants.kGains.kPShooter, Constants.TIMEOUT_MS);
-		shooterMotor.config_kI(Constants.PID_LOOP_IDX, Constants.kGains.kIShooter, Constants.TIMEOUT_MS);
-    shooterMotor.config_kD(Constants.PID_LOOP_IDX, Constants.kGains.kDShooter, Constants.TIMEOUT_MS);*/
+		leftFrontDriveMotor.config_kF(AutoConstants.AUTO_PID_LOOP_IDX, Constants.kGains.kFShooter, AutoConstants.AUTO_TIMEOUT_MS);  
+    leftFrontDriveMotor.config_kP(AutoConstants.AUTO_PID_LOOP_IDX, Constants.kGains.kPShooter, AutoConstants.AUTO_TIMEOUT_MS);
+		leftFrontDriveMotor.config_kI(AutoConstants.AUTO_PID_LOOP_IDX, Constants.kGains.kIShooter, AutoConstants.AUTO_TIMEOUT_MS);
+    leftFrontDriveMotor.config_kD(AutoConstants.AUTO_PID_LOOP_IDX, Constants.kGains.kDShooter, AutoConstants.AUTO_TIMEOUT_MS);*/
 
   }
   /*public void m_shoot()
@@ -67,14 +71,14 @@ double rpm = 6380;
     //600 is a modifer to get min to 100 ms and 2048 gets rotations to units 
     //Right now I'm putting the motors at desired rpm for testing purposes 6380 or whatever number is after (2048 / 600) will change to rpm
 
-    shooterMotor.set(TalonFXControlMode.Velocity, -motorSpeed);
+    leftFrontDriveMotor.set(TalonFXControlMode.Velocity, -motorSpeed);
 
-   SmartDashboard.putNumber("RPM", ( (600.0 / Constants.K_SENSOR_UNITS_PER_ROTATION) * shooterMotor.getSelectedSensorVelocity()));
+   SmartDashboard.putNumber("RPM", ( (600.0 / Constants.K_SENSOR_UNITS_PER_ROTATION) * leftFrontDriveMotor.getSelectedSensorVelocity()));
    
    String motorState;
-   if(shooterMotor.getSelectedSensorVelocity(Constants.PID_LOOP_IDX) > 0){
+   if(leftFrontDriveMotor.getSelectedSensorVelocity(AutoConstants.AUTO_PID_LOOP_IDX) > 0){
      motorState = "forward";
-   }else if(shooterMotor.getSelectedSensorVelocity(Constants.PID_LOOP_IDX) == 0){
+   }else if(leftFrontDriveMotor.getSelectedSensorVelocity(AutoConstants.AUTO_PID_LOOP_IDX) == 0){
      motorState = "stopped";
    }else{
      motorState = "reverse";
@@ -85,15 +89,15 @@ double rpm = 6380;
   }
   public void m_stopSpinning()
   {
-    shooterMotor.set(TalonFXControlMode.PercentOutput, 0.0);
+    leftFrontDriveMotor.set(TalonFXControlMode.PercentOutput, 0.0);
   }
 
   public void m_zeroEncoder(){
-    shooterMotor.setSelectedSensorPosition(0);
+    leftFrontDriveMotor.setSelectedSensorPosition(0);
   }
 
   public double m_getPosition(){
-    return shooterMotor.getSelectedSensorPosition();
+    return leftFrontDriveMotor.getSelectedSensorPosition();
   }
 
   @Override
