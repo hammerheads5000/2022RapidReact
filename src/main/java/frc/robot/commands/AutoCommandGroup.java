58896f774410af1762cc2f2a7b595d5336ec097d
Blanch121 <24ablanch@hinghamschools.org>
2022-02-14 16:07:36 -0500

    Changed one line

---
+++ src/main/java/frc/robot/commands/AutoCommandGroup.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoCommandGroup extends SequentialCommandGroup {
+  /** Creates a new AutoCommandGroup. */
+  public AutoCommandGroup() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    
+    /* Route Templete
+    addCommands(
+    Turn,
+    Fire,
+    Command
+    );
+    */
+
+    /* Route Templete
+    addCommands(
+    Turn,
+    Fire,
+    Command
+    );
+    */
+
+
+  }
+}
